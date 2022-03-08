0ecdb0eee727815fb54a09948de94d73827f239f
[srikanth] <[tallapallisrikanth1159@gmail.com]>
2022-03-08 16:15:31 +0530

    updationg regex

--- Regular_Expressions/programe1.py
+++ Regular_Expressions/programe1.py
@@ -2,8 +2,10 @@
 certain set of characters (in this case a-z, A-Z and 0-9)."""
 import re
 text = """In123 publishing and graphic design, Lorem123 Ipsum is Placeholder 
-        text commonly used to demonstrate the Visual form of a document 
+        text commonly used to demon78strate the Visual form of a document 
         or a typeface without relying on meaningful content."""
-x=re.search("[a-zA-Z0-9]*", text)
+x = re.search("\w*", text)
 print(x.group())
+y = re.findall("[A-Za-z]+[0-9]+", text)
+print(y)
 
